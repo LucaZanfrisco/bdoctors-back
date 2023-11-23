c612df584f886062a344113cbcc020a4ea1fb088
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-11-23 17:44:59 +0100

    Scaffolding e creazione prime tabelle db

---
+++ database/migrations/2023_11_23_152958_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->unique();
+            $table->text('cv');
+            $table->string('description');
+            $table->text('services');
+            $table->string('address');
+            $table->text('photo');
+            $table->boolean('visible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
