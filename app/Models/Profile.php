618a4b86410b015e952e2492e7891ca8a3702454
Christian01881 <chrispamo01881@gmail.com>
2023-11-24 15:44:08 +0100

    Written Link Types in Models

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -16,4 +16,20 @@ class Profile extends Model
     public function messages(){
         return $this->hasMany(Message::class);
     }
+
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+
+    public function sponsorships(){
+        return $this->belongsToMany(Sponsorship::class);
+    }
+
+    public function stars(){
+        return $this->belongsToMany(Star::class);
+    }
+
+    public function typologies(){
+        return $this->belongsToMany(Typology::class);
+    }
 }
