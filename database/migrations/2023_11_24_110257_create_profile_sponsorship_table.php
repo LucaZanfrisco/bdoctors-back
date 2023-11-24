0c3aa8ed6539390c21a18e22e0de74c09a16f3cf
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-11-24 12:19:50 +0100

    Creazione delle tabelle nel DB

---
+++ database/migrations/2023_11_24_110257_create_profile_sponsorship_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile_sponsorship', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('profile_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('sponsorship_id')->constrained()->cascadeOnDelete();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile_sponsorship');
+    }
+};
